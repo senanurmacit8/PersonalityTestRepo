46995ebbcf37515746f2c172547a15ff8690ce38
senanurmacit <senamacit8@gmail.com>
2022-04-12 22:50:49 +0300

    fixed

--- src/main/java/com/questions_application/questions_application_0_0_1/service/bussiness/DocumentServices.java
+++
@@ -1,9 +0,0 @@
-package com.questions_application.questions_application_0_0_1.service.bussiness;
-
-public interface DocumentServices {
-
-    public void getDocument();
-
-    public void saveDocuments();
-
-}
